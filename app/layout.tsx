7df4e554fb87e2feaecd60b4bda7e6d3f3c6622d
Milad Roghanian <miladr100@gmail.com>
2025-11-05 02:42:22 +0000

    feat: add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TGKM9XBP');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGKM9XBP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <TooltipProvider>
           <div className="flex flex-col min-h-screen">
             <Header />
